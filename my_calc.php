99223972da294305e4430f8a70a97e745e806751
PrashaMeena <prashameena97@gmail.com>
2019-02-21 15:01:10 +0530

    Update my_calc.php

--- my_calc.php
+++ my_calc.php
@@ -2,8 +2,11 @@
   <head> My first page
   </head>
   <body>
+    <form>
+        Enter your name: <input type="text" name="u_name"/>
     <?php
       echo "Hello";
     ?>
+    </form>
   </body>
 </html>
